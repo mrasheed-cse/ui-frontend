f75bd093f7efeea20bae058f2a3cf0e22bdf10a7
Syed Yousuf Ali <sayed.ali1@wipro.com>
2019-04-23 03:25:50 +0600

    Report

---
+++ src/app/views/nsa/models/BulkReports.js
@@ -0,0 +1,14 @@
+export class BulkReports {
+     
+	msisdn: string; 
+    definitionStatus: string; 
+    definitionCompletionDate: string; 
+    provisionStatus: string; 
+    provisionCompletionDate: string; 
+    deProvisionStatus: string; 
+	deProvisionCompletionDate: string;
+    reProvisionhStatus: string; 
+    reProvisionCompletionDate: string; 
+    mnpProvisionStatus: string; 
+	mnpProvisionCompletionDate: string;
+}
--- src/app/views/nsa/services/report.service.ts
+++ src/app/views/nsa/services/report.service.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { environment } from '../../../../environments/environment.prod';
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/retry';
+import 'rxjs/add/observable/of';
+
+import { AppGlobals } from './../../../app.global';
+
 
 @Injectable()
 export class ReportService {
 
-  constructor() { }
+  serverUrl: string;
+
+  constructor(private router: Router, private http: HttpClient, private _global: AppGlobals) {
+    this.serverUrl = environment.apiUrl;
+  }
+
+  SingleNumberReport(msisdn: string) : any {	
+  
+    console.log("In SingleNumberReport() for MSISDN "+ msisdn);
+    
+		return this.http.post(this.serverUrl + 'SingleNumberReport', {
+      msisdn: msisdn
+		});	  
+  }
+  
+  MultipleNumberReport(startMSISDN: string, endMSISDN: string) : any {	
+  
+    console.log("In MultipleNumberReport() for startMSISDN: "+ startMSISDN+" and endMSISDN: "+endMSISDN);
+    
+		return this.http.post(this.serverUrl + 'MultipleNumberReport', {
+      start: startMSISDN,
+      end: endMSISDN
+		});	  
+	}
 
 }
