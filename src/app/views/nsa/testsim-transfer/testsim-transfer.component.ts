983fbfc38e77cd6aaae179d238be3980844d8d31
faria.hasan <faria.hasan@wipro.com>
2019-08-28 15:43:58 +0600

    transfer ok

--- src/app/views/nsa/testsim-transfer/testsim-transfer.component.ts
+++ src/app/views/nsa/testsim-transfer/testsim-transfer.component.ts
@@ -105,11 +105,77 @@ export class TestsimTransferComponent implements OnInit {
 
   submit(){
 
+    for(var i = 0; i < this.requisitionList.length; i++){
+
+      //todo hard code
+
+      /*if(this.requisitionList[i]['rechargeAmount'] == null ||
+      this.requisitionList[i]['rechargeAmount'] == undefined ||
+      this.requisitionList[i]['rechargeAmount'] == ""){
+          var msg = "At row " + (i+1) + " recharge amount is blank.";
+          alert(msg);
+          return;
+      }*/
+
+      if(this.requisitionList[i]['comments'] == null ||
+      this.requisitionList[i]['comments'] == undefined ||
+      this.requisitionList[i]['comments'] == ""){
+          var msg = "At row " + (i+1) + " comments is blank.";
+          alert(msg);
+          return;
+      }
+
+    }
+
+    ////////////// ////////////////////////
+
+    this.isLoading = true;
+
+    var requestObj = {};
+    requestObj['defWorkRequestId'] = this._global.wrid_testSimTransfer;
+    requestObj['initiator'] = this.userName;
+    requestObj['initiateDate'] = "";
+    requestObj['requisitionId'] = 0;
+    requestObj['numberWiseDetails'] = [];
+
+    for(var i = 0; i < this.requisitionList.length; i++){
+
+      var requestDetailObj = {};
+      requestDetailObj['requisitionLineMsisdnId'] = this.requisitionList[i]['requisitionLineMsisdnId'];
+      requestDetailObj['justification'] = this.requisitionList[i]['comments'];
+      requestDetailObj['comments'] = this.requisitionList[i]['comments'];
+      requestDetailObj['newCreditLimit'] = 0;
+      requestDetailObj['newEndDate'] = "";
+      requestDetailObj['rechargeAmount'] = 0;
+      requestDetailObj['transferMode'] = this.requisitionList[i]['transferMode'];
+      requestDetailObj['transferTo'] = this.requisitionList[i]['transferTo'];
+      requestDetailObj['lostDamageMode'] = "";
+      requestDetailObj['lostDamageDate'] = "";
+
+      requestObj['numberWiseDetails'].push(requestDetailObj);
+
+    } //end of loop over numbers
+
+    ////////////////// /////////////////////////////////
+    this.workFlowsService.submitSimActionRequest(requestObj).subscribe(
+      data => {
+        
+      },
+    err => console.error(err),
+    () => console.log('Done loading PendingTask List')
+    );
+    setTimeout(()=>{    //<<<---    using ()=> syntax
+      this.isLoading = false;
+      alert("The request has been submitted");
+      this.router.navigate(['nsa/testsimdashboard']);
+    }, 4000);
+
+    ////////////// /////////////////////// /////////////
   }
 
   cancel(){
     this.router.navigate(['nsa/testsimdashboard']);
-  }  
+  }
   
   //////////// ///////////////////////////// //////////////////////////////////
   
