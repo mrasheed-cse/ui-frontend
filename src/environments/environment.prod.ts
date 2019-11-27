84a1609a953cb01b97bdd8039c36fdf68b76c5ee
Afifa Julia <afifa.julia@wipro.com>
2019-11-27 17:01:23 +0600

    dsf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
    //production: true,
    production: false,
   //apiUrl: 'https://10.10.23.51:8444/nsa/', //prod
-  //apiUrl: 'http://10.10.20.86:8081/',  
-  apiUrl: 'http://localhost:8019/',
+  //apiUrl: 'https://10.10.23.50:8443/nsa/', //pre-prod
+  apiUrl: 'http://localhost:8059/nsa',
   pendingRequestMarker : 'PENDING',
   approvedRequestMarker : 'APPROVED',
   rejectedRequestMarker : 'REJECTED',
