0c60ce316d00a6af264fdb6dbc31861c03e3d00a
Kumar challa <challa.kumarreddy@wipro.com>
2020-10-04 16:20:21 +0600

    Create Upload in Msisdn

--- src/app/views/nsa/uploadrecycle.component.ts
+++ src/app/views/nsa/uploadrecycle.component.ts
@@ -9,28 +9,43 @@ import { MsisdnService } from '../nsa/msisdn.service';
 export class UploadrecycleComponent implements OnInit {
 
   fileToUpload: File = null;
-  fileenable:boolean=false;
-  constructor( private msisdnService: MsisdnService) { }
+  fileenable: boolean = false;
+  fileuploadstatus: string;
+  filestatus: boolean = false;
+  constructor(private msisdnService: MsisdnService) { }
   ngOnInit(): void {
   }
   dndUpload() {
     if (this.fileToUpload == undefined || this.fileToUpload == null) {
-     this.fileenable=true;
+      this.fileenable = true;
     } else {
-   this.msisdnService.postFile(this.fileToUpload).subscribe((res)=>{
-     console.log(res);
-     this.fileenable=false;
-   },err=>{
-    
-   })
+      this.msisdnService.postFile(this.fileToUpload).subscribe((res) => {
+        console.log(res);
+        this.fileenable = false;
+      }, err => {
+
+      })
     }
   }
   handleFileInput(files: FileList) {
-    this.fileenable=false;
+    this.fileenable = false;
+    this.filestatus = false;
     this.fileToUpload = files.item(0);
+    this.msisdnService.postFile(this.fileToUpload).subscribe((res => {
+      if (res == null) {
+        this.fileuploadstatus = 'File Upload Fail';
+        this.filestatus = true;
+      } else {
+        this.fileuploadstatus = 'File Upload Success';
+        this.filestatus = true;
+      }
+    }), err => {
+      this.fileuploadstatus = 'File Upload Fail';
+      this.filestatus = true;
+    })
     console.log(this.fileToUpload.size);
   }
 
-  
+
 
 }
