7c44f4f02cd56f5469d102e65ff410b1719d7bc4
Sadikullah Zobair <mohammad.sadikullahzobair@wipro.com>
2021-02-06 23:30:20 +0600

    fix generate list page business logics

--- src/app/views/nsa/msisdn.service.ts
+++ src/app/views/nsa/msisdn.service.ts
@@ -1,35 +1,28 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Misidn } from '../../views/nsa/misidn';
-import { environment } from '../../../environments/environment';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Misidn} from '../../views/nsa/misidn';
+import {environment} from '../../../environments/environment';
 import {Observable} from 'rxJs';
+
 @Injectable()
 export class MsisdnService {
+    constructor(private httpClient: HttpClient) {
+    }
 
-  constructor(private httpClient: HttpClient) { }
+    getAnalyzeData(st: any, ed: any): Observable<Misidn[]> {
+        let url = environment.apiUrl + "msisdn_recycle_list_analyze/start/" + st + "/" + ed;
+        return this.httpClient.get<Misidn[]>(url);
+    }
 
+    generateMsisdnList(st: any, ed: any): Observable<string> {
+        let url = environment.apiUrl + "msisdn_recycle_list_generate/start/" + st + "/" + ed;
+        return this.httpClient.get<string>(url, {responseType: "text" as 'json'});
+    }
 
- 
-  
-  
-  getAnalyzeData(st:any,ed:any):Observable<Misidn[]>{
-    let url=environment.apiUrl + "msisdn_recycle_list_analyze/start/" + st + "/" + ed;
-    console.log(url);
-    return this.httpClient.get<Misidn[]>(url); 
-}
-
-getGenerateData(st:any,ed:any):Observable<Misidn[]>{
-
-  let url=environment.apiUrl + "msisdn_recycle_list_generate/start/" + st + "/" + ed;
-  console.log(url);
-  return this.httpClient.get<Misidn[]>(url); 
-}
-
-  postFile(fileToUpload: File) {
-    const url = environment.apiUrl + "msisdn_recycle_list_upload/start";
-    const formData: FormData = new FormData();
-    formData.append('file', fileToUpload, fileToUpload.name);
-    return this.httpClient.post(url, formData);
-
-  }
+    postFile(fileToUpload: File) {
+        const url = environment.apiUrl + "msisdn_recycle_list_upload/start";
+        const formData: FormData = new FormData();
+        formData.append('file', fileToUpload, fileToUpload.name);
+        return this.httpClient.post(url, formData);
+    }
 }
