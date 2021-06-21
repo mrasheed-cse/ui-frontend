b849166e7f3704bdd33271c97665fe8531ff35ee
Viplove Pandey <viplove.pandey@wipro.com>
2021-06-21 20:27:52 +0600

    Removed clear button from generatelist

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: false,
-	apiUrl: '/nsa',
+	apiUrl: '/',
 	pendingRequestMarker: 'PENDING',
 	approvedRequestMarker: 'APPROVED',
 	rejectedRequestMarker: 'REJECTED',
