4c0c0a1c052eca3253893bc106d2833b41705d96
Afifa Julia <afifa.julia@wipro.com>
2021-07-08 12:39:52 +0600

    Asynchronization is included in AuthGuard.service.ts to control the dynamic group change in page

--- src/app/views/nsa/de-provision-details.component.ts
+++ src/app/views/nsa/de-provision-details.component.ts
@@ -64,7 +64,7 @@ import {
 	constructor(private loginService: LoginService,private activatedRoute: ActivatedRoute, private router:Router, public _global: AppGlobals, private workFlowsService: WorkflowsService, private fileoperationService: FileoperationService) {
 		
 	  // Get Current User Profile
-	  
+	  console.log("hello");
 	  this.currentLoggedInUser = this.loginService.GetCurrentLoggedInUser();
 	  
 	  if (this.currentLoggedInUser) {
@@ -94,10 +94,9 @@ import {
 	LoadQueryStringData(){
 		// LOAD QUERY STRING DATA
 		  this.wr_BriefId = Number(this.activatedRoute.snapshot.paramMap.get('wr_BriefId'));
-		  console.log(this.wr_BriefId);
-		  
+		 // console.log(this.wr_BriefId);		  
 		  this.hop_sequence = Number(this.activatedRoute.snapshot.paramMap.get('hopSequence'));		
-		  console.log(this.hop_sequence);
+		  //console.log(this.hop_sequence);
 		  
 		  this.wrBriefName = this.activatedRoute.snapshot.paramMap.get('wr_BriefName');
 	  
@@ -114,7 +113,6 @@ import {
 					  console.log(totalData);
 					  if (totalData%2==1){   
 						  console.log("totalData is odd");
-						  
 						  this.fieldNameValueList.push({fieldName: "", fieldValue: ""});
   
 					  }
@@ -129,7 +127,7 @@ import {
 				  */				
 			  },
 			  err => console.error(err),
-			  () => console.log('Done loading LoadPreviousHopsField List')
+			  () => console.log('Done loading LoadPreviousHopsField List ******')
 			  );  
   
   }
@@ -148,7 +146,7 @@ import {
 		   if(this.hop_sequence==4) // LAST HOP IN De-PROVISION
 			  this.isDone = true;
 			this.workFlowsService.UpdateExistiongWorkRequest(this.workFlowsService.FormatWorkRequestNameForAPI(this.wrBriefName),this._global.wrid_DeProvisioning, this.groupID,this.userID,this.hop_sequence,"",this.isDone).subscribe(
-		res  =>  {
+res=>{
 		  console.log('response is : '+res.message);
 		  
 		
@@ -170,7 +168,7 @@ if(this.hop_sequence!==4){
 				  this.successAlertMessage = " has been completed successfully.";
 		  }
 		},
-		err  =>  {		  
+	err => {		  
 			console.log("err.status : "+err.status);		  
 			this.dangerAlertShow = true;
 		  this.dangerAlertMessage = " .";
