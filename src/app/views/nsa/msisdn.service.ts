dee071d0679172698946d2f2e69224b81c7f77a0
Viplove Pandey <viplove.pandey@wipro.com>
2021-07-12 15:54:11 +0600

    Fixed CI/CD Building issue changed models name #3

--- src/app/views/nsa/msisdn.service.ts
+++ src/app/views/nsa/msisdn.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {RecycleCandidateMsisdn} from './recycleCandidateMsisdn';
 import {environment} from '../../../environments/environment';
-import {Observable} from 'rxJs';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class MsisdnService {
