ced1d61a0599b6e16b3cca13b6e4a51e360c9a02
Sadikullah Zobair <mohammad.sadikullahzobair@wipro.com>
2021-10-07 02:57:09 +0600

    change ui for data warehouse management

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: false,
-	apiUrl: '/',
+	apiUrl: '/gmg',
 	pendingRequestMarker: 'PENDING',
 	approvedRequestMarker: 'APPROVED',
 	rejectedRequestMarker: 'REJECTED',
