908b7580a6cac6de23c836de6dd4451cee2de3e5
Afifa Julia <afifa.julia@wipro.com>
2022-02-22 14:40:37 +0600

    Approval page removed New page came with the option to download the files which were given for batch testing

--- src/app/views/SSM/ScratchCard/voucher_generation.component.ts
+++ src/app/views/SSM/ScratchCard/voucher_generation.component.ts
@@ -111,6 +111,11 @@ export class VoucherGeneration implements OnInit {
 submitVoucher(){
 	this.isLoading=true;
 	console.log(this.voucherGenrationForm.controls.Po.value)
+	if(this.voucherGenrationForm.controls.Po.value==''||(this.voucherGenrationForm.controls.Po.value)){
+		alert("Please provide the Item No(PO)");
+		this.isLoading=false;
+		return;
+	}
 	this.ssmService.checkPoExsist(this.voucherGenrationForm.controls.Po.value).subscribe(
 		data =>{
 			this.isLoading=false;
