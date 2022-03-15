82b8c9271bf9cfaa1937e747d703cf1c51dd1c87
Afifa Julia <afifa.julia@wipro.com>
2022-03-15 14:35:46 +0600

    auc conversion fixation

--- src/app/views/SSM/DataWarehouse Management/uploaddata.component.ts
+++ src/app/views/SSM/DataWarehouse Management/uploaddata.component.ts
@@ -58,33 +58,64 @@ export class UploadDataWh implements OnInit {
 submit(){
 	var val;
 	this.isLoading=true;
-	console.log("Selected for"+this.uploadFor)
+	console.log("Selected for "+this.uploadFor)
 	if(this.uploadFor!=null){if(this.uploadFor==="0"){
 		val="AUC";
 	}
 	else if(this.uploadFor==="1"){
 		val="ADC"
 	}
-	
+	/*
+	//console.log(data);
+			const dataStr = JSON.stringify(data);
+
+			JSON.parse(dataStr, (key, value) => {
+				if (typeof value === 'string') {
+					this.WR_Name = value;
+					return value;
+				}
+			}); */
 	this.datawarehouseservice.uploadCsv(this.fileToUpload,val).subscribe(
 	
-		data=>{ 	
-			console.log(data)
+		data=>{ 
+			//alert(data);	
+			//console.log(data)
+			//console.log(data);
+			const dataStr = JSON.stringify(data);
+
+			JSON.parse(dataStr, (key, value) => {
+				if (typeof value === 'string') {
+					alert(value);
+				}
+			});
 			if(data!=null){
-				console.log("Data Sucessfully Saved")
-			
+
+			alert(data);
 			alert("Data Saved  Sucessfully"); this.fileerror = false;
         this.filesuccess = false;
         this.fileToUpload = null;
         this.isLoading=false
 		}
 		 else{
-			alert("Please Select Valid File")
+			alert("respnse is null")
 			 this.isLoading=false
 		}
 		
-		},err => {alert("Please Select Valid File")
-		 this.isLoading=false}
+		},err => {
+			
+			//alert('err');
+			const dataStr = JSON.stringify(err);
+			JSON.parse(dataStr, (key, value) => {
+				
+				if (typeof value === 'string' ) {
+					if(key==='text'){
+						////alert(key+" : "+value);
+						alert(value);
+					}
+				}
+			});
+		 this.isLoading=false
+		}
 	)
 	}
 	else{
