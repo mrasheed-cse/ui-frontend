82705715146bfcab557a5b4f61780363f5d40a6d
afifa <afifa.julia@wipro.com>
2022-08-21 15:11:32 +0600

    solved:: multiple replacement KIT series msisdn 0 could not be assigned again

--- src/app/views/nsa/requisitiondetailsdelivery/requisitiondetailsdelivery.component.ts
+++ src/app/views/nsa/requisitiondetailsdelivery/requisitiondetailsdelivery.component.ts
@@ -324,10 +324,13 @@ export class RequisitiondetailsdeliveryComponent implements OnInit {
                     return;
                   }
                   if(res[y]['mobile_No'] == this.finalArrayToSubmit[z].msisdnInfo[z1]['mobile_No']){
+                    if(res[y]['mobile_No']!='0'){
                     let innerAlertMsg = "The MSISDN " + res[y]['mobile_No'] + " has been assigned already in a previous line item. Please use new MSISDN.";
                     alert(innerAlertMsg);
                     return;
+                    }
                   }
+                  
                   if(res[y]['imsi_No'] == this.finalArrayToSubmit[z].msisdnInfo[z1]['imsi_No']){
                     let innerAlertMsg = "The IMSI number " + res[y]['imsi_No'] + " has been assigned already in a previous line item. Please specify new IMSI number.";
                     alert(innerAlertMsg);
