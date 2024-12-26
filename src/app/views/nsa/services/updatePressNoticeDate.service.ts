16ee37d14cf5b92b7c2ce67017490059d1a0d86d
genex.rezaul <rezaul.karim@genexinfosys.com>
2024-12-26 12:46:31 +0600

    Initial commit: Recycle number master list upload to Database Upload the MSISDN list for Tagging as MFS and send email with attachment Upload the MSISDN list for De-Tagging as MFS and send email with attachment List number wise Press notice date update

---
+++ src/app/views/nsa/services/updatePressNoticeDate.service.ts
@@ -0,0 +1,39 @@
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+import {Constants} from 'ag-grid-community';
+import {AppGlobals} from 'app/app.global';
+import {environment} from 'environments/environment';
+
+@Injectable()
+export class UpdatePressNoticeDateService {
+    private headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
+
+    private options = {
+        headers: this.headers,
+        responseType: 'text' as 'json'
+    }
+
+    serverUrl: string;
+
+    constructor(private router: Router, private http: HttpClient, private _global: AppGlobals) {
+        this.serverUrl = environment.apiUrl;
+    }
+
+    getListIds(): any {
+        const url = environment.apiUrl + 'GetListIds';
+        return this.http.get(url);
+    }
+
+    updatePressNoticeDate(userId: string, listId: string, date: string): any {
+        const url = environment.apiUrl + 'UpdatePressNoticeDate';
+
+        let requestData = {
+            createdBy: userId,
+            listId: listId,
+            date: date
+        }
+        return this.http.post(url, requestData);
+    }
+
+}
