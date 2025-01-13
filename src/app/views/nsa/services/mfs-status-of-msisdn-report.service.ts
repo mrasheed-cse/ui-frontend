e07849a3b75506cc4e65e2758ce37d3e5af05b8a
genex.rezaul <rezaul.karim@genexinfosys.com>
2025-01-13 19:00:17 +0600

    MSISDN Search

---
+++ src/app/views/nsa/services/mfs-status-of-msisdn-report.service.ts
@@ -0,0 +1,24 @@
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+import {AppGlobals} from 'app/app.global';
+import {environment} from 'environments/environment';
+
+@Injectable()
+export class MfsStatusOfMsisdnReportService {
+
+    serverUrl: string;
+
+    constructor(private router: Router, private http: HttpClient, private _global: AppGlobals) {
+        this.serverUrl = environment.apiUrl;
+    }
+
+
+    searchByMsisdn(msisdn: string): any {
+        const url = environment.apiUrl + 'get-mfs-status-of-msisdn';
+
+        const params = new HttpParams().set('msisdn', msisdn);
+
+        return this.http.get(url, {params});
+    }
+}
